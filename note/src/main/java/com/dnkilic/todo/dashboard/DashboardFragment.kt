3a9b2a41d3370c6ccc3c5ee7856081cd8cc836a4
Dogan Kilic <dnkilic@outlook.com>
2019-09-01 18:55:56 +0700

    Design the UI for Dashboard screen

--- note/src/main/java/com/dnkilic/todo/dashboard/DashboardFragment.kt
+++ note/src/main/java/com/dnkilic/todo/dashboard/DashboardFragment.kt
@@ -1,6 +1,10 @@
 package com.dnkilic.todo.dashboard
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProviders
 import com.dnkilic.todo.R
 import com.dnkilic.todo.core.base.BaseFragment
@@ -8,6 +12,7 @@ import com.dnkilic.todo.dashboard.viewmodel.DashboardViewModel
 import com.dnkilic.todo.dashboard.viewmodel.DashboardViewModelFactory
 import com.dnkilic.todo.data.NotesDependencyHolder
 import javax.inject.Inject
+
 
 class DashboardFragment : BaseFragment() {
 
@@ -24,7 +29,28 @@ class DashboardFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
         component.inject(this)
         viewModel.getNotes()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        val searchView = SearchView((activity as DashboardActivity).supportActionBar?.themedContext)
+        menu.findItem(R.id.action_search).apply {
+            setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW or MenuItem.SHOW_AS_ACTION_IF_ROOM)
+            actionView = searchView
+        }
+
+        searchView.queryHint = getString(R.string.search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
     }
 }
